71d6a99b8a1ab49ff29de0322ef7ea15ebe79b31
Özcan Kara <ozcankara53@gmail.com>
2021-01-20 20:54:58 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle (Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : " + musteri.MusteriAdi);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Silinen Müşterinin Adı : " + musteri.MusteriAdi);
+        }
+
+        public void MusteriListele(Musteri musteri1, Musteri musteri2)
+        {
+            Console.WriteLine("1. Müşteri : " + musteri1.MusteriAdi);
+            Console.WriteLine("2. Müşteri : " + musteri2.MusteriAdi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAdi = "Engin ";
+            musteri1.MusteriSoyadi = "Demirog";
+            musteri1.MusteriAdres = "Hocam Sok. No:1";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.MusteriAdi = "Özcan";
+            musteri2.MusteriSoyadi = "Kara";
+            musteri2.MusteriAdres = "Öğrenci Sok. No:2";
+
+            //Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine( "Ekleme Bölümü --------");
+            
+            musteriManager.MusteriEkle(musteri1);
+
+            Console.WriteLine("Silme Bölümü --------");
+            musteriManager.MusteriSil(musteri2);
+
+            Console.WriteLine("Listeleme Bölümü --------");
+            musteriManager.MusteriListele(musteri1,musteri2);
+
+        }
+    }
+}
